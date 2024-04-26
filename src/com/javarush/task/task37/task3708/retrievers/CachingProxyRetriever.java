f8dc67f83fddc44a38fd18aa396a1b1a0886f1e8
Dimash <mwasikzano@gmail.com>
2024-04-27 01:44:42 +0500

    JREx7

---
+++ src/com/javarush/task/task37/task3708/retrievers/CachingProxyRetriever.java
@@ -0,0 +1,2 @@
+package com.javarush.task.task37.task3708.retrievers;public class CachingProxyRetriever {
+}
---
+++ src/com/javarush/task/task37/task3710/decorators/RedShapeDecorator.java
@@ -0,0 +1,2 @@
+package com.javarush.task.task37.task3710.decorators;public class RedShapeDecorator {
+}
